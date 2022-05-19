9784a1754c3f78eacc158c93073149c6faf77562
ajaypanasa <ajay.kk@panasatech.com>
2022-05-20 00:22:38 +0530

    second commit new

--- onbeat-qa-automation-android/src/main/java/qa/mobile/test1.java
+++ onbeat-qa-automation-android/src/main/java/qa/mobile/test1.java
@@ -42,7 +42,7 @@ public class test1 {
 
         String appurl = File.separator + "home" + File.separator + "runner" + File.separator
                 + "work" + File.separator + "onbeatcicd" + File.separator + "onbeatcicd"
-                + File.separator + "app" + File.separator + "build" + File.separator + "outputs" + File.separator + "apk" + File.separator + "debug" + File.separator";
+                + File.separator + "app" + File.separator + "build" + File.separator + "outputs" + File.separator + "apk" + File.separator + "debug" + File.separator;
 
 //        File appDir = new File("src/main/resources/");
 //        File app = new File(appDir, "app-debug.apk");
