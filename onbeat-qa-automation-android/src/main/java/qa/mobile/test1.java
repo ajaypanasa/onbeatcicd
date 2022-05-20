cf715139adcb1276c23eba94e71f262c9bc926ea
ajaypanasa <ajay.kk@panasatech.com>
2022-05-20 09:41:11 +0530

    friday commit

--- onbeat-qa-automation-android/src/main/java/qa/mobile/test1.java
+++ onbeat-qa-automation-android/src/main/java/qa/mobile/test1.java
@@ -52,9 +52,9 @@ public class test1 {
         String urlDir = "/home/runner/work/onbeatcicd/onbeatcicd/onbeat-qa-automation-android/src/main/resources/";
 
         String fullPath = "/home/runner/work/onbeatcicd/onbeatcicd/onbeat-qa-automation-android/src/main/resources/app-debug.apk";
-        Atring newpath = "../app/build/outputs/apk/debug/app-debug.apk"
+        Atring newpath = "../app/build/outputs/apk/debug/app-debug.apk";
 
-        File appDir = new File(urlDir);
+        File appDir = new File("src/main/resources/");
         File app = new File(appDir, "app-debug.apk");
 
        // '/home/runner/work/onbeatcicd/onbeatcicd/onbeat-qa-automation-android/src/main/resources/app-debug.apk
@@ -65,7 +65,7 @@ public class test1 {
 //        caps.setCapability("appPackage", "www.onbeatapps.com");
 //        caps.setCapability("appActivity", "www.onbeatapps.com.ui.authActivity.AuthActivity");
 
-        caps.setCapability(MobileCapabilityType.APP, newpath);
+        caps.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
 
        // caps.setCapability(MobileCapabilityType.APP, appurl);
         URL url = new URL("http://127.0.0.1:4723/wd/hub");
